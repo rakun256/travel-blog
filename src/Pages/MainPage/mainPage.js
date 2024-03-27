b0bd28b0a81defb1867fc231995c470e3ec27976
Emre Uslu <emreuslu256@gmail.com>
2024-03-27 22:43:46 +0300

    some SEO

--- src/Pages/MainPage/mainPage.js
+++ src/Pages/MainPage/mainPage.js
@@ -3,10 +3,31 @@ import MainSegment from '../../Segments/MainSegment/mainSegment'
 import LastPosts from '../../Segments/LastPosts/lastPosts'
 import './mainPage.css'
 import Footer from '../../Components/Footer/footer'
+import { Helmet } from 'react-helmet';
 
 const MainPage = () => {
   return (
     <div className='main-page-cover'>
+      <Helmet>
+      <title>Main Page - Adventurista</title>
+        <meta name="description" content="Welcome, adventurers! We are Adventurista, a group of mathematics
+          engineering students who have embarked on journeys to explore the
+          world. We have a passion for discovering the captivating beauty of
+          nature, the profound traces of history, and the richness of cultures.
+          Every moment we open the door to a new adventure, we feel the
+          excitement in our hearts, and we're here to share that excitement with
+          you. Adventurista was founded with the aim of sharing our adventures
+          that take us to unique destinations around the globe. Here you can
+          find our experiences, inspiring stories, and practical tips gathered
+          during our travels. Our articles serve as guides for travel
+          enthusiasts, designed to inspire those who wish to explore new places.
+          We emphasize not only the importance of geographical exploration but
+          also the significance of building bridges between people and engaging
+          in cultural exchange. Because we believe that traveling not only
+          allows us to see new places but also brings us closer together as
+          global citizens. Welcome to Adventurista. Are you ready to explore the
+          world together?" />
+      </Helmet>
         <MainSegment />
         <div className='lastposts-cover'>
         <LastPosts />
